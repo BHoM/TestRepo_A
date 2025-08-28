5feb626f10367f5fc1fd9b30a4dae8f0cf3212b0
Isak Näslund <isak.naslund@burohappold.com>
2025-08-28 14:15:12 +0200

    Update ExampleObject.cs

--- TestA_oM/ExampleObject.cs
+++ TestA_oM/ExampleObject.cs
@@ -29,7 +29,7 @@ using System.Linq;
 namespace BH.oM.TestA
 {
     [Description("Object description in here. Will appear in the UI tooltip.")]
-    public class ExampleObject : IExampleInterfaceA
+    public class ExampleObjectA : IExampleInterfaceA
     {
         // // See examples in the BHoM repo and the wiki to see how we define types.
         // // Generally, all properties should be public and have public getter and setter.
