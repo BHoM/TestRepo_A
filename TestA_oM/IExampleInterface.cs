65ac3ce1f0061c0310038a5870e549f9f4a8f7ee
Isak Näslund <isak.naslund@burohappold.com>
2025-09-02 11:26:13 +0200

    Update IExampleInterface.cs

--- TestA_oM/IExampleInterface.cs
+++ TestA_oM/IExampleInterface.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace BH.oM.TestA
 {
-    public interface IExampleInterface : IObject
+    public interface IExampleInterface : IObject 
     {
         string SomeStringProperty { get; set; } 
     }
